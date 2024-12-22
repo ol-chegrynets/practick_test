29706c45a34ae9444d875a21e997d3f891630099
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2024-12-22 15:40:17 +0200

    add exenge form

--- src/components/ExchangeForm/ExchangeForm.jsx
+++ src/components/ExchangeForm/ExchangeForm.jsx
@@ -1,14 +1,32 @@
 import { RiExchangeDollarFill } from 'react-icons/ri';
 import styles from './ExchangeForm.module.css';
+import { useDispatch } from 'react-redux';
+import { fetchExchangeInfo } from '../../redux/currency/operrations';
 
 export const ExchangeForm = () => {
+  const dispatch = useDispatch();
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { value } = e.target.elements.currency;
+    console.log(value);
+    const [amount, from, , to] = value.split(' ');
+    dispatch(fetchExchangeInfo({ amount, from, to }));
+  };
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <button className={styles.button} type="submit">
         <RiExchangeDollarFill className={styles.icon} />
       </button>
 
-      <input title="Request format 15 USD in UAH" className={styles.input} />
+      <input
+        type="text"
+        pattern="^\d+(\.\d{1,2})?\s[a-zA-Z]{3}\sin\s[a-zA-Z]{3}$"
+        placeholder="15 USD in UAH"
+        title="Request format 15 USD in UAH"
+        className={styles.input}
+        name="currency"
+        required
+      />
     </form>
   );
 };
